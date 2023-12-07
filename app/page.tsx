8635e264114ce5e618bda43136d396088b77933e
dogthepet <122819243+dogthepet@users.noreply.github.com>
2023-12-07 20:26:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import redirections from "../redirects.json"
+import { redirect } from "next/navigation";
 
-export const config = {
-  runtime: 'edge',
-};
-
-export default async (req) => {
-  const [subdomain] = req.headers.get('x-forwarded-host').split(`.`);
-  const redirection = redirections[subdomain] || 'https://dogthepet.com/frodo-the-last-survivor-of-michael-vicks-dogfighting-ring-r-i-p/?'; // Reemplazá con tu link por defecto
-
-  return Response.redirect(redirection, 302);
-};
+export default function Home() {
+  console.log("Redirecting from home");
+  redirect("https://dogthepet.com/frodo-the-last-survivor-of-michael-vicks-dogfighting-ring-r-i-p/");
+}
