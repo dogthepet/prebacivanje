59fef156bac68c0a9f82fddde7b05971ebb41791
dogthepet <122819243+dogthepet@users.noreply.github.com>
2023-12-07 20:25:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   console.log("Redirecting from home");
-  redirect("http://google.com");
+  redirect("https://dogthepet.com/frodo-the-last-survivor-of-michael-vicks-dogfighting-ring-r-i-p/");
 }
